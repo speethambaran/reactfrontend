7ec65b7ada6388c04680fabc410e9127bd6d3565
amaljamesgit <amaljamesambalamkunnu@gmail.com>
2022-11-16 23:59:52 -0700

    login and  register screen

--- envitusplatformfrontend/src/screens/LoginScreen.js
+++ envitusplatformfrontend/src/screens/LoginScreen.js
@@ -1,11 +1,67 @@
-import React from 'react'
+import React, { Component} from "react";
+import "./Register.css"
 
-function LoginScreen() {
-  return (
-    <div>
-      <h1>LoginScreen</h1>
-    </div>
-  );
+
+class  LoginScreen extends Component {
+    constructor() {
+        super()
+        this.state = ({ username: '', password: '' })
+
+    }
+
+    usernamechange = event => {
+        this.setState({
+            username: event.target.value
+        })
+    }
+    passwordchange = event => {
+        this.setState({
+            password: event.target.value
+        })
+    }
+    handleSubmit = event => {
+
+        alert(`${this.state.username}`);
+
+
+    }
+
+    render() {
+
+
+        return <body>
+
+            <div className="container">
+
+                <form action="" onSubmit={this.handleSubmit} >
+                    <div className="cover">
+                        <h1>Signin</h1>
+                       
+                        <input type="text" placeholder="username" name="username" value={this.state.username} onChange={this.usernamechange} required /><br />
+                        <input type="password" placeholder="password" name="password" value={this.state.password} onChange={this.passwordchange} required /><br />
+                        <button className="signup-btn" type="submit">
+                            Login
+                        </button>
+
+                        <p>Not a User?<a href="/register">Register</a></p>
+                    </div>
+                </form>
+
+            </div>
+
+        </body >
+    }
 }
+export default  LoginScreen
 
-export default LoginScreen
+// import React from 'react'
+
+// function() {
+//   return (
+//     <div>
+//       <h1>LoginScreen</h1>
+//     </div>
+//   );
+// }
+
+// export default LoginScreen
