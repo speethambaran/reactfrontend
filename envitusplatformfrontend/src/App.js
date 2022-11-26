5fb7dc977a9ca38b093d617bd2f5878cf22cac4e
Aswin-S-T <aswinswebart15@gmail.com>
2022-11-26 10:28:53 +0530

    amal branch merged and conflit removed

--- envitusplatformfrontend/src/App.js
+++ envitusplatformfrontend/src/App.js
@@ -1,6 +1,5 @@
-<<<<<<< HEAD
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
 // import Header from './components/Header';
 // import Dashboard from './components/Dashboard';
 import HomeScreen from './screens/HomeScreen';
@@ -13,42 +12,6 @@ import Topbar from "./screens/global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
-
-  return (
-    <BrowserRouter>
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <main className="content">
-          <Topbar />
-          <Routes>
-           
-           <Route path="/" element={<HomeScreen />} />
-          <Route path='/login' element={<LoginScreen/>} />
-          <Route path='/register' element={<RegisterScreen />} />
-          <Route path='/chart' element={<Chart />} />
-
-            
-            </Routes>
-            
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-    </BrowserRouter>
-=======
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-// import Dashboard from './components/Dashboard';
-import HomeScreen from "./screens/HomeScreen";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import { useNavigate } from "react-router-dom";
-import Chart from "./components/Chart";
-
-function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
   useEffect(() => {
@@ -60,49 +23,27 @@ function App() {
     }
   }, []);
   return (
-    <div>
-      {/* <header>
-        <Header />
-      </header> */}
-      <main>
-        <Routes>
-          <Route path="/dashboard" element={<HomeScreen />} />
-          <Route path="/" exact={true} element={<LoginScreen />} />
-          <Route path="/register" element={<RegisterScreen />} />
-          <Route path="/chart" element={<Chart />} />
-          {/* <Route path="users/*" element={<Users />} /> */}
-        </Routes>
-      </main>
-    </div>
->>>>>>> 6d3322cb5e01d9b13b21ede72ec2c6296b9eafc0
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+          <Topbar />
+          <Routes>
+           
+           <Route path="/dashboard" element={<HomeScreen />} />
+          <Route path='/login' element={<LoginScreen/>} />
+          <Route path='/register' element={<RegisterScreen />} />
+          <Route path='/chart' element={<Chart />} />
+
+            
+            </Routes>
+            
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
 export default App;
-<<<<<<< HEAD
-
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-
-//     {/* <header>
-//       <Header />
-//     </header> */}
-//       <main>
-//         <Routes>
-//           <Route path="/" element={<HomeScreen />} />
-//           <Route path='/login' element={<LoginScreen/>} />
-//           <Route path='/register' element={<RegisterScreen />} />
-//           <Route path='/chart' element={<Chart />} />
-//           {/* <Route path="users/*" element={<Users />} /> */}
-//         </Routes>
-//       </main>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App
-
-=======
->>>>>>> 6d3322cb5e01d9b13b21ede72ec2c6296b9eafc0
